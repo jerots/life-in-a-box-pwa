0539be4fa86e76f34bfcada29e7025da48c4b946
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2020-01-01 02:31:18 +0800

    add variable boxes + tooltip showing year

--- src/components/main-display.vue
+++ src/components/main-display.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="main-container">
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app_fill"></f7-icon>
-    <f7-icon f7="app"></f7-icon>
-    <f7-icon f7="app"></f7-icon>
-    <f7-icon f7="app"></f7-icon>
+    <f7-icon
+      v-for="filled in currAge"
+      :tooltip="String(filled)"
+      :key="'filled-' + filled"
+      f7="app_fill"
+    ></f7-icon>
+    <f7-icon
+      v-for="unfilled in lifeLeft"
+      :tooltip="String(unfilled + currAge)"
+      :key="'unfilled-' + unfilled"
+      f7="app"
+    ></f7-icon>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currAge: 28,
+      lifeExpectancy: 80
+    };
+  },
+  computed: {
+    lifeLeft() {
+      return this.lifeExpectancy - this.currAge;
+    }
+  }
+};
+</script>
 
 <style scoped>
 .main-container {
