59454e9535462ef397d9e29a79ef7545eeaeb617
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2020-01-01 02:18:02 +0800

    add placeholder boxes

---
+++ src/components/main-display.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main-container">
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app_fill"></f7-icon>
+    <f7-icon f7="app"></f7-icon>
+    <f7-icon f7="app"></f7-icon>
+    <f7-icon f7="app"></f7-icon>
+  </div>
+</template>
+
+<style scoped>
+.main-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.life-unit {
+}
+</style>
